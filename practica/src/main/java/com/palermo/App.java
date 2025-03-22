eec55c3d9418c79dc59850c9bdeaecc06eb91d73
vdacun1 <vdacun1@palermo.edu>
2025-03-22 10:20:47 -0300

    Proyecto de práctica

---
+++ practica/src/main/java/com/palermo/App.java
@@ -0,0 +1,13 @@
+package com.palermo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
